8891fff2dd4caa3f626718b80a9699b70a1a0a95
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-16 21:43:26 +0700

    add function for DiamondService

--- Services/Abstraction/IDiamondServices.cs
+++ Services/Abstraction/IDiamondServices.cs
@@ -1,6 +1,11 @@
-﻿namespace Services.Abstraction
+﻿using DTO.DiamondDto;
+
+namespace Services.Abstraction
 {
     public interface IDiamondServices
     {
+        Task<DiamondResponse> CreateDiamondAsync(DiamondRequest diamondRequest);
+
+        Task<IEnumerable<DiamondResponse>> GetAllAsync(CancellationToken cancellationToken);
     }
 }
--- Services/DiamondServices.cs
+++ Services/DiamondServices.cs
@@ -1,8 +1,51 @@
-﻿using Services.Abstraction;
+﻿using AutoMapper;
+using BusinessObject.Models;
+using DataAccessLayer.Abstraction;
+using DTO.DiamondDto;
+using Repositories.Abstraction;
+using Services.Abstraction;
 
 namespace Services
 {
     public class DiamondServices : IDiamondServices
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IDiamondRepository _diamondRepository;
+        private readonly IPaperworkRepository _paperworkRepository;
+        private readonly IMapper _mapper;
+
+        public DiamondServices(IDiamondRepository diamondRepository, IPaperworkRepository paperworkRepository, IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _diamondRepository = diamondRepository;
+            _paperworkRepository = paperworkRepository;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<DiamondResponse> CreateDiamondAsync(DiamondRequest diamondRequest)
+        {
+            var diamond = _mapper.Map<Diamond>(diamondRequest);
+
+            _diamondRepository.Add(diamond);
+            _paperworkRepository.AddRange(diamond.PaperWorks);
+
+            await _unitOfWork.SaveChangeAsync();
+
+            return _mapper.Map<DiamondResponse>(diamond);
+        }
+
+        public async Task<IEnumerable<DiamondResponse>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            var diamonds = await _diamondRepository.Find(
+                diamond => diamond.IsSold != true && diamond.IsDeleted != true,
+                cancellationToken,
+                diamond => diamond.PaperWorks,
+                diamond => diamond.Media,
+                diamond => diamond.Promotion,
+                diamond => diamond.DiamondAccessories
+            );
+
+            return _mapper.Map<IEnumerable<DiamondResponse>>(diamonds);
+        }
     }
 }
