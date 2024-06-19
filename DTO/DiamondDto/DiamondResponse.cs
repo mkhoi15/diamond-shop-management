0cad86b53654936fda774f6cbedede7f82ce41b9
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-19 17:15:54 +0700

    add paperworkDto modify DiamondDto

--- DTO/DiamondDto/DiamondResponse.cs
+++ DTO/DiamondDto/DiamondResponse.cs
@@ -2,5 +2,16 @@
 {
     public class DiamondResponse
     {
+        public string? Origin { get; set; }
+        public string? Color { get; set; }
+        public string? Cut { get; set; }
+        public string? Clarity { get; set; }
+        public string? Weight { get; set; }
+        public decimal? Price { get; set; }
+        public Guid? MediaId { get; set; }
+        public bool? IsSold { get; set; }
+        public Guid? PromotionId { get; set; }
+        public string? MediaUrl { get; set; }
+        public string? PromotionDescription { get; set; }
     }
 }
