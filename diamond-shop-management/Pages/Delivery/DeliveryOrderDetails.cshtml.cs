385e398431e85bd82b1fbad3b6e175ed6086d85c
mkhoi15 <khoivmse171254@fpt.edu.vn>
2024-07-17 20:35:58 +0700

    Delivery done

---
+++ diamond-shop-management/Pages/Delivery/DeliveryOrderDetails.cshtml.cs
@@ -0,0 +1,40 @@
+using DTO.OrderDto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Abstraction;
+
+namespace diamond_shop_management.Pages.Delivery;
+
+public class DeliveryOrderDetails : PageModel
+{
+    private readonly IOrderServices _orderServices;
+
+    public DeliveryOrderDetails(IOrderServices orderServices)
+    {
+        _orderServices = orderServices;
+    }
+    
+    public OrderResponse Order { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(Guid id, CancellationToken cancellationToken)
+    {
+        try
+        {
+            Order = await _orderServices.GetOrderByIdAsync(id, cancellationToken);
+            
+            var lastDelivery = Order.Deliveries.LastOrDefault();
+
+            if (Order == null)
+            {
+                return NotFound("Not found order");
+            }
+
+            return Page();
+
+        }
+        catch (Exception e)
+        {
+            return NotFound("An error occurred while retrieving the order details.");
+        }
+    }
+}
